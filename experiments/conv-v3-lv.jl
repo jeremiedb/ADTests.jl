fda5aea7e31c6a747394d740b2f4ade5295b517d
jeremie.db <jeremie.db@evovest.com>
2024-01-02 01:45:15 -0500

    up

--- experiments/conv-v3-lv.jl
+++ experiments/conv-v3-lv.jl
@@ -1,31 +1,33 @@
-using Revise
+# using Revise
 # using Random: seed!
 # using Base.Threads: @threads
 using LoopVectorization
 using Enzyme
+using Random: seed!
 # using BenchmarkTools
 
 ####################################################################################################################################
 # Skip LV to see if Enzyme can handle it
 ####################################################################################################################################
-# function my_conv_A(x, w)
-#     y = zero(x)
-#     for hi in axes(x, 2)
-#         for k in axes(x, 1)
-#             y[k] += w[k] * x[k, hi]
-#         end
-#     end
-#     return y
-# end
-# x = rand(Float32, 3, 5);
-# w = rand(Float32, 3);
-# y = my_conv_A(x, w);
-# loss_A(x, w) = sum(my_conv_A(x, w))
-# dw = zero(w);
-# loss_A(x, w)
+function my_conv_A(x, w)
+    y = zero(x)
+    for hi in axes(x, 2)
+        for k in axes(x, 1)
+            y[k] += w[k] * x[k, hi]
+        end
+    end
+    return y
+end
+seed!(123)
+x = rand(Float32, 3, 5);
+w = rand(Float32, 3);
+y = my_conv_A(x, w);
+loss_A(x, w) = sum(my_conv_A(x, w))
+dw = zero(w);
+loss_A(x, w)
 # @code_warntype loss_A(x, w)
-# grads = Enzyme.autodiff(Reverse, loss_A, Const(x), Duplicated(w, dw));
-
+grads = Enzyme.autodiff(Reverse, loss_A, Const(x), Duplicated(w, dw));
+@info dw
 
 function my_conv_B(x, w)
     y = zero(x)
@@ -36,6 +38,7 @@ function my_conv_B(x, w)
     end
     return y
 end
+seed!(123)
 x = rand(Float32, 3, 5);
 w = rand(Float32, 3);
 y = my_conv_B(x, w);
@@ -44,3 +47,4 @@ dw = zero(w);
 loss_B(x, w)
 # @code_warntype loss_B(x, w)
 grads = Enzyme.autodiff(Reverse, loss_B, Const(x), Duplicated(w, dw));
+@info dw
