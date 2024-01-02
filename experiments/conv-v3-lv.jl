9dd4f5d7d12735d14b4f344859d366c545bed6eb
jeremie <jeremie.desgagne.bouchard@gmail.com>
2024-01-02 01:44:55 -0500

    up

--- experiments/conv-v3-lv.jl
+++ experiments/conv-v3-lv.jl
@@ -8,24 +8,26 @@ using Enzyme
 ####################################################################################################################################
 # Skip LV to see if Enzyme can handle it
 ####################################################################################################################################
-# function my_conv_A(x, w)
-#     y = zero(x)
-#     for hi in axes(x, 2)
-#         for k in axes(x, 1)
-#             y[k] += w[k] * x[k, hi]
-#         end
-#     end
-#     return y
-# end
-# x = rand(Float32, 3, 5);
-# w = rand(Float32, 3);
-# y = my_conv_A(x, w);
-# loss_A(x, w) = sum(my_conv_A(x, w))
-# dw = zero(w);
-# loss_A(x, w)
-# @code_warntype loss_A(x, w)
-# grads = Enzyme.autodiff(Reverse, loss_A, Const(x), Duplicated(w, dw));
 
+function my_conv_A(x, w)
+    y = zero(x)
+    for hi in axes(x, 2)
+        for k in axes(x, 1)
+            y[k] += w[k] * x[k, hi]
+        end
+    end
+    return y
+end
+
+x = rand(Float32, 3, 5);
+w = rand(Float32, 3);
+y = my_conv_A(x, w);
+
+loss_A(x, w) = sum(my_conv_A(x, w))
+dw = zero(w);
+loss_A(x, w)
+@code_warntype loss_A(x, w)
+grads = Enzyme.autodiff(Reverse, loss_A, Const(x), Duplicated(w, dw));
 
 function my_conv_B(x, w)
     y = zero(x)
